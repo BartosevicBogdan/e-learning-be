401e5843e1d735f2feec2f9a02b697295e34154d
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-05-22 19:36:20 +0300

    Feat: Index.js. Auth and profile routes added to server file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-
 const { port } = require('./config');
 
 const app = express();
 
+const authRoute = require('./routes/v1/AuthRoute');
+const profileRoute = require('./routes/v1/ProfileRoute');
+
+//middleware
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+//HTTP request handlers
+app.get('/ping', (req, res) => {
   res.send({ msg: 'Server is running' });
 });
+
+app.use('/v1/auth', authRoute);
+app.use('/v1/profile', profileRoute);
 
 app.all('*', (req, res) => {
   res.status(404).send({ error: 'Page not found' });
